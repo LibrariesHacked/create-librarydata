64fc20066681bf28f23d426ccadd36dd512653a0
Dave <DaveBathnes@users.noreply.github.com>
2019-12-16 22:22:15 +0000

    #7 Form for postcode to LSOA. Closes #7

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <b-field>
+      <b-upload
+        type="is-light"
+        accept=".csv"
+        v-on:input="$emit('upload-file', $event)"
+        drag-drop
+      >
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon icon="upload" size="is-large"> </b-icon>
+            </p>
+            <p>Drop your CSV file here or click to add</p>
+          </div>
+        </section>
+      </b-upload>
+    </b-field>
+
+    <div class="tags">
+      <span class="tag is-primary" v-if="file">
+        {{ file.name }}
+        <button
+          class="delete is-small"
+          type="button"
+          @click="$emit('delete-file')"
+        ></button>
+      </span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["file"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
--- src/views/PostcodeToLsoa.vue
+++ src/views/PostcodeToLsoa.vue
@@ -10,17 +10,88 @@
         </div>
       </div>
     </section>
+    <div class="container">
+      <br />
+      <b-steps
+        type="is-info"
+        size="is-medium"
+        :has-navigation="false"
+        v-model="active_step"
+        :animated="true"
+      >
+        <b-step-item label="Choose File" icon="cloud-upload">
+          <file-upload
+            v-bind:file="file"
+            v-on:upload-file="file = $event"
+            v-on:delete-file="file = null"
+          />
+          <br />
+          <b-button
+            size="is-large"
+            icon-right="chevron-right"
+            @click="confirmFile"
+            :disabled="file === null"
+          >
+            Next
+          </b-button>
+          <br />
+          <br />
+        </b-step-item>
+        <b-step-item label="Options" icon="settings">
+          <b-field label="Select postcode column">
+            <b-select placeholder="Select column" v-model="postcode_column">
+              <option v-for="option in columns" :value="option" :key="option">
+                {{ option }}
+              </option>
+            </b-select>
+          </b-field>
+          <br />
+          <b-button
+            size="is-large"
+            icon-right="chevron-right"
+            @click="confirmOptions"
+            :disabled="postcode_column === ''"
+          >
+            Convert
+          </b-button>
+        </b-step-item>
+        <b-step-item label="Download" icon="cloud-download"></b-step-item>
+      </b-steps>
+    </div>
     <custom-footer />
   </div>
 </template>
 <script>
 import Footer from "../components/Footer";
+import FileUpload from "../components/FileUpload";
+
+import * as Papa from "papaparse";
 
 export default {
   data() {
-    return {};
+    return {
+      active_step: 0,
+      columns: [],
+      file: null,
+      postcode_column: ""
+    };
   },
-  methods: {},
-  components: { "custom-footer": Footer }
+  methods: {
+    confirmFile: function() {
+      if (this.file !== null) {
+        let self = this;
+        Papa.parse(self.file, {
+          complete: function(results) {
+            self.columns = results.data[0];
+            self.active_step = 1;
+          }
+        });
+      }
+    },
+    confirmOptions: function() {
+      this.active_step = 2;
+    }
+  },
+  components: { "custom-footer": Footer, "file-upload": FileUpload }
 };
 </script>

70c2b322dec950cbd51bf18115ea787634eaa792
Dave <DaveBathnes@users.noreply.github.com>
2021-02-10 07:47:57 +0000

    Updates

---
+++ precache-manifest.88ef4a82af65a9a2cf90294c7083f076.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5706bb1668356773c8b4bf9ce8bebc76",
+    "url": "/404.html"
+  },
+  {
+    "revision": "95bdd270e235e884f4777c7169882807",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "2a4aa266abdcc58d1fc4",
+    "url": "/css/app.0baf85ee.css"
+  },
+  {
+    "revision": "9dad3f13c1a95b5f8022",
+    "url": "/css/chunk-vendors.a9d4f839.css"
+  },
+  {
+    "revision": "34201d253db67f3dd72371c9e04f41aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4aa266abdcc58d1fc4",
+    "url": "/js/app.49057aee.js"
+  },
+  {
+    "revision": "eda01629215946cbb8ff",
+    "url": "/js/chunk-2d22497b.38f4de6c.js"
+  },
+  {
+    "revision": "9dad3f13c1a95b5f8022",
+    "url": "/js/chunk-vendors.e0b6301f.js"
+  },
+  {
+    "revision": "22a770b833177363d5fcb2bc4a9e3c10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "abf765827dd636c1cdb45f871d0c05d6",
+    "url": "/style.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.454be17d4a6f99e4d6611fc3ee7f5ba3.js"
+  "/precache-manifest.88ef4a82af65a9a2cf90294c7083f076.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "create-librarydata"});

67bbf754f19598e2a36d113e20ee45927b1a2e04
Dave <DaveBathnes@users.noreply.github.com>
2021-12-21 08:23:32 +0000

    #36 Adding service select component

---
+++ src/components/ServiceSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <v-select
+      :items="items"
+      label="Outlined style"
+      outlined
+      :value="value"
+      @change="update"
+    ></v-select>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    update(newValue) {
+      this.$emit("change", newValue);
+    }
+  }
+};
+</script>

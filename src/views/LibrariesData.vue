d9d63931222acb3d0316a079d02903c568908b2a
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-02-25 09:46:11 +0000

    #52 Removing new line at end of saved file - odd behaviour

--- src/views/LibrariesData.vue
+++ src/views/LibrariesData.vue
@@ -786,7 +786,7 @@ export default {
       this.downloadFile(`${service.code}_libraries.csv`, this.libraries);
     },
     downloadFile: function (filename, data) {
-      var csv = new Blob([Papa.unparse(data) + '\r\n'], {
+      var csv = new Blob([Papa.unparse(data)], {
         type: "text/csv;charset=utf-8;"
       });
       if (navigator.msSaveBlob) {
@@ -811,7 +811,7 @@ export default {
           librariesToPublish[i][newKey] = val;
         });
       });
-      const csvData = new Blob([Papa.unparse(librariesToPublish) + '\r\n'], {
+      const csvData = new Blob([Papa.unparse(librariesToPublish)], {
         type: "text/csv;charset=utf-8;"
       });
       const saveResult = await schemaHelper.saveSchemaFile(

81e87d850f55734cd79adf9ba8f13a7fb630b6e0
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-01-07 10:09:18 +0000

    Updating packages and adding carbon badge (#68)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import store from "./plugins/store";
+import CarbonBadge from 'vue-carbonbadge'
+Vue.use(CarbonBadge)
 
 Vue.config.productionTip = false;
 

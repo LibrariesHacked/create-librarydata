9cb8bf4f530ce1b950345a222265c6b028480a2a
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-05-03 20:25:51 +0100

    Updating library editor popup

--- src/components/service-select.vue
+++ src/components/service-select.vue
@@ -1,19 +1,17 @@
 <template>
   <v-card elevation="0" color="grey-lighten-5">
-    <v-card-text>
-      <div class="text-caption pa-3">Select local authority</div>
-      <v-autocomplete persistent-hint bg-color="green" class="elevation-0" clearable :v-model="value"
-        :items="libraryServices" item-title="nice_name" item-value="code" label="Find a local authority" return-object
-        :loading="libraryServices.length === 0" prepend-inner-icon="mdi-domain" v-on:update:modelValue="updateSelection"
-        hint="You can type to search. After selecting a service, select the arrow icon to confirm">
-        <template v-slot:append>
-          <v-icon :color="selectedService !== null ? 'success' : 'grey'"
-            :icon="serviceConfirmed ? 'mdi-check' : 'mdi-chevron-right-circle'" disabled="false"
-            @click="confirmSelection()"></v-icon>
-        </template>
-      </v-autocomplete>
-      <v-chip v-if="serviceConfirmed" class="ma-2" closable @click:close="serviceConfirmed = false"></v-chip>
-    </v-card-text>
+    <div class="text-caption pa-3">Select local authority</div>
+    <v-autocomplete persistent-hint bg-color="green" class="elevation-0" clearable :v-model="value"
+      :items="libraryServices" item-title="nice_name" item-value="code" label="Find a local authority" return-object
+      :loading="libraryServices.length === 0" prepend-inner-icon="mdi-domain" v-on:update:modelValue="updateSelection"
+      hint="You can type to search. After selecting a service, select the arrow icon to confirm">
+      <template v-slot:append>
+        <v-icon :color="selectedService !== null ? 'success' : 'grey'"
+          :icon="serviceConfirmed ? 'mdi-check' : 'mdi-chevron-right-circle'" disabled="false"
+          @click="confirmSelection()"></v-icon>
+      </template>
+    </v-autocomplete>
+    <v-chip v-if="serviceConfirmed" class="ma-2" closable @click:close="serviceConfirmed = false">{{selectedService.nice_name}}</v-chip>
   </v-card>
 </template>
 
--- src/views/libraries-data.vue
+++ src/views/libraries-data.vue
@@ -15,7 +15,6 @@
     <h2 class="text-h5 text-decoration-underline my-3">Edit library locations</h2>
 
     <v-sheet color="grey-lighten-5" rounded elevation="0" class="px-5 py-5">
-
       <v-alert class="my-2" icon="mdi-numeric-1-circle"
         text="Choose a local authority to view the libraries within that area."></v-alert>
 
@@ -24,8 +23,7 @@
       <v-alert class="my-2" icon="mdi-numeric-2-circle" text="To make changes select the edit icon for each library.
         Changes are not saved until you publish changes."></v-alert>
 
-      <v-card color="grey-lighten-2" variant="outlined" elevation="0" class="mx-3 my-2">
-
+      <v-card color="grey-lighten-2" variant="outlined" elevation="0">
         <v-data-table hover items-per-page="5"
           no-data-text="To load library data ensure you select a local authority in Step 1." :headers="headers"
           :items="libraries" :sort-by="[{ key: 'Library_name', order: 'asc' }]" class="elevation-0">
@@ -118,8 +116,9 @@
                           </v-row>
                           <v-row dense>
                             <v-col cols="12" sm="12" md="12">
-                              <v-text-field density="compact" variant="outlined" v-model="editedItem.Unique_property_reference_number
-                                " label="Unique property reference number"></v-text-field>
+                              <v-text-field density="compact" variant="outlined"
+                                v-model="editedItem.Unique_property_reference_number"
+                                label="Unique property reference number"></v-text-field>
                             </v-col>
                           </v-row>
                         </v-container>
@@ -128,29 +127,30 @@
                         <v-container fluid>
                           <v-row dense>
                             <v-col cols="12" sm="6" md="6" v-for="hourType in openingHourTypes" :key="hourType">
-                              <div v-for="day in days" :key="day">
+                              <h4 class="text-subtitle">{{ hourType }}</h4>
+                              <div v-for="day in days" :key="day" class="ma-2">
+                                <v-divider/>
                                 <div class="text-subtitle-2">
-                                  <v-btn icon color="success" v-on:click="addOpeningHoursEntry(
-                                      day + '_' + hourType + '_hours'
-                                    )
-                                    ">
-                                    <v-icon>mdi-plus-circle-outline</v-icon>
+                                  {{ day }}
+                                  <v-btn variant="tonal" icon="mdi-plus-circle" size="x-small" color="success" v-on:click="addOpeningHoursEntry(
+                                    day + '_' + hourType.toLowerCase() + '_hours'
+                                  )">
                                   </v-btn>
-                                  {{ day }} {{ hourType }}
                                 </div>
 
                                 <v-chip class="ma-1" v-for="session in editedItem[
-                                  day + '_' + hourType + '_hours'
+                                  day + '_' + hourType.toLowerCase() + '_hours'
                                 ]
                                   ? editedItem[
-                                    day + '_' + hourType + '_hours'
+                                    day + '_' + hourType.toLowerCase() + '_hours'
                                   ].split(',')
                                   : ''" :key="session" small label close close-icon="mdi-delete" color="success"
                                   v-on:click:close="removeOpeningHoursEntry(
-                                      day + '_' + hourType + '_hours',
+                                      day + '_' + hourType.toLowerCase() + '_hours',
                                       session
                                     )
                                     ">{{ session }}</v-chip>
+
                               </div>
                             </v-col>
                           </v-row>
@@ -174,8 +174,9 @@
                 <v-card>
                   <v-card-title>Remove library?</v-card-title>
                   <v-card-text>
-                    This action will remove the library. If the library has closed it should not be removed. The
-                    closed year can be entered in the library details to mark it as closed and retain the details.
+                    This action will remove the library. If the library has closed it
+                    should not be removed. The closed year can be entered in the library
+                    details to mark it as closed and retain the details.
                   </v-card-text>
                   <v-card-actions>
                     <v-spacer />
@@ -232,9 +233,7 @@
           </template>
           <template v-slot:[`item.actions`]="{ item }">
             <div class="d-flex">
-              <v-icon class="mr-2" v-on:click="editItem(item.raw)">
-                mdi-pencil
-              </v-icon>
+              <v-icon class="mr-2" v-on:click="editItem(item.raw)"> mdi-pencil </v-icon>
               <v-icon v-on:click="deleteItem(item.raw)">mdi-delete</v-icon>
             </div>
           </template>
@@ -242,18 +241,18 @@
       </v-card>
 
       <v-alert class="my-2" icon="mdi-numeric-3-circle">
-        To finish you can save a copy of the data to your computer. If you are
-        logged in with access to edit the library authority data, you can
-        overwrite the existing data by selecting <strong>Publish changes</strong>.
+        To finish you can save a copy of the data to your computer. If you are logged in
+        with access to edit the library authority data, you can overwrite the existing
+        data by selecting <strong>Publish changes</strong>.
       </v-alert>
 
-      <v-btn :disabled="!isEditing" size="large" variant="tonal" color="info" class="ma-2" append-icon="mdi-content-save"
+      <v-btn :disabled="!isEditing" size="large" variant="tonal" color="info" append-icon="mdi-content-save"
         v-on:click="download">
         Save local copy
       </v-btn>
 
-      <v-btn :disabled="!isEditing" size="large" variant="tonal" color="success" append-icon="mdi-cloud-upload"
-        v-on:click="publishChanges">
+      <v-btn :disabled="!isEditing" size="large" variant="tonal" color="success" class="ma-2"
+        append-icon="mdi-cloud-upload" v-on:click="publishChanges">
         Publish changes
       </v-btn>
 
@@ -263,9 +262,7 @@
           <v-card-text>Your changes have been published. Thank you</v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="secondary" text @click="successDialog = false">
-              Close
-            </v-btn>
+            <v-btn color="secondary" text @click="successDialog = false"> Close </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -277,53 +274,50 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="secondary" text @click="errorDialog = false">
-              Close
-            </v-btn>
+            <v-btn color="secondary" text @click="errorDialog = false"> Close </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-
     </v-sheet>
   </v-container>
 </template>
 
 <script>
-import Header from "../components/layout-header";
-import ServiceSelect from "../components/service-select";
+import Header from '../components/layout-header'
+import ServiceSelect from '../components/service-select'
 
-import * as Papa from "papaparse";
-import * as schemaHelper from "../helpers/schemas";
+import * as Papa from 'papaparse'
+import * as schemaHelper from '../helpers/schemas'
 
-import Markdown from "../components/markdown-section";
-import MarkDownData from "../markdown/librariesdata.md";
+import Markdown from '../components/markdown-section'
+import MarkDownData from '../markdown/librariesdata.md'
 
-const config = require("../helpers/config.json");
+const config = require('../helpers/config.json')
 
 export default {
   beforeMount() {
-    window.addEventListener("beforeunload", event => {
+    window.addEventListener('beforeunload', event => {
       if (!this.isEditing) return
       event.preventDefault()
       // Chrome requires returnValue to be set.
-      event.returnValue = ""
+      event.returnValue = ''
     })
   },
   beforeDestroy() {
-    window.removeEventListener("beforeunload", this.preventNav);
+    window.removeEventListener('beforeunload', this.preventNav)
   },
   data() {
     return {
       days: [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday'
       ],
-      openingHourTypes: ["staffed", "unstaffed"],
+      openingHourTypes: ['Staffed', 'Unstaffed'],
       dialogMainLibrary: false,
       dialogOpeningHours: false,
       dialogDelete: false,
@@ -332,53 +326,53 @@ export default {
       editTabs: 1,
       editedItem: {},
       defaultItem: {
-        Local_authority: "",
-        Library_name: "",
-        Address_1: "",
-        Address_2: "",
-        Address_3: "",
-        Postcode: "",
-        Unique_property_reference_number: "",
-        Statutory: "",
-        Type_of_library: "LAL",
-        Year_opened: "",
-        Year_closed: "",
-        Monday_staffed_hours: "",
-        Tuesday_staffed_hours: "",
-        Wednesday_staffed_hours: "",
-        Thursday_staffed_hours: "",
-        Friday_staffed_hours: "",
-        Saturday_staffed_hours: "",
-        Sunday_staffed_hours: "",
-        Monday_unstaffed_hours: "",
-        Tuesday_unstaffed_hours: "",
-        Wednesday_unstaffed_hours: "",
-        Thursday_unstaffed_hours: "",
-        Friday_unstaffed_hours: "",
-        Saturday_unstaffed_hours: "",
-        Sunday_unstaffed_hours: "",
-        Special_hours: "",
-        Colocated: "",
-        Colocated_with: "",
-        Notes: "",
-        URL: "",
-        Email_address: ""
+        Local_authority: '',
+        Library_name: '',
+        Address_1: '',
+        Address_2: '',
+        Address_3: '',
+        Postcode: '',
+        Unique_property_reference_number: '',
+        Statutory: '',
+        Type_of_library: 'LAL',
+        Year_opened: '',
+        Year_closed: '',
+        Monday_staffed_hours: '',
+        Tuesday_staffed_hours: '',
+        Wednesday_staffed_hours: '',
+        Thursday_staffed_hours: '',
+        Friday_staffed_hours: '',
+        Saturday_staffed_hours: '',
+        Sunday_staffed_hours: '',
+        Monday_unstaffed_hours: '',
+        Tuesday_unstaffed_hours: '',
+        Wednesday_unstaffed_hours: '',
+        Thursday_unstaffed_hours: '',
+        Friday_unstaffed_hours: '',
+        Saturday_unstaffed_hours: '',
+        Sunday_unstaffed_hours: '',
+        Special_hours: '',
+        Colocated: '',
+        Colocated_with: '',
+        Notes: '',
+        URL: '',
+        Email_address: ''
       },
       errorDialog: false,
       headers: [
         {
-          title: "Name",
-          align: "start",
-          value: "Library_name"
+          title: 'Name',
+          align: 'start',
+          value: 'Library_name'
         },
-        { title: "Closed", value: "Year_closed" },
+        { title: 'Closed', value: 'Year_closed' },
         {
-          title: "Address 1",
-          align: "start",
-          value: "Address_1"
+          title: 'Address 1',
+          align: 'start',
+          value: 'Address_1'
         },
-        { title: "Postcode", value: "Postcode" },
-        { title: "Actions", key: "actions", sortable: false }
+        { title: 'Postcode', value: 'Postcode' },
+        { title: 'Actions', key: 'actions', sortable: false }
       ],
       isEditing: false,
       libraries: [],
@@ -392,140 +386,140 @@ export default {
       openingHoursClose: null,
       selectedService: null,
       successDialog: false
-    };
+    }
   },
   methods: {
     editItem(item) {
-      this.editedIndex = this.libraries.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogMainLibrary = true;
+      this.editedIndex = this.libraries.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogMainLibrary = true
     },
     deleteItem(item) {
-      this.editedIndex = this.libraries.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
+      this.editedIndex = this.libraries.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
     },
     deleteItemConfirm() {
-      this.isEditing = true;
-      this.libraries.splice(this.editedIndex, 1);
-      this.closeDelete();
+      this.isEditing = true
+      this.libraries.splice(this.editedIndex, 1)
+      this.closeDelete()
     },
     close() {
-      this.dialogMainLibrary = false;
+      this.dialogMainLibrary = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedItem.Local_authority = this.selectedService.name;
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedItem.Local_authority = this.selectedService.name
+        this.editedIndex = -1
+      })
     },
     closeDelete() {
-      this.dialogDelete = false;
+      this.dialogDelete = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
     },
     addOpeningHoursEntry(key) {
-      this.openingHoursEditKey = key;
-      this.dialogOpeningHoursEntry = true;
+      this.openingHoursEditKey = key
+      this.dialogOpeningHoursEntry = true
     },
     removeOpeningHoursEntry(key, session) {
-      var sessions = this.editedItem[key].split(",").filter((x) => x.length > 0);
-      sessions.splice(sessions.indexOf(session), 1);
-      this.editedItem[key] = sessions.join(",");
-      this.dialogOpeningHoursEntry = false;
+      var sessions = this.editedItem[key].split(',').filter(x => x.length > 0)
+      sessions.splice(sessions.indexOf(session), 1)
+      this.editedItem[key] = sessions.join(',')
+      this.dialogOpeningHoursEntry = false
     },
     closeOpeningHoursEntry() {
-      this.dialogOpeningHoursEntry = false;
+      this.dialogOpeningHoursEntry = false
     },
     confirmOpeningHoursEntry() {
       var sessions = this.editedItem[this.openingHoursEditKey]
-        .split(",")
-        .filter((x) => x.length > 0);
-      sessions.push(`${this.openingHoursOpen}-${this.openingHoursClose}`);
-      this.editedItem[this.openingHoursEditKey] = sessions.join(",");
-      this.dialogOpeningHoursEntry = false;
+        .split(',')
+        .filter(x => x.length > 0)
+      sessions.push(`${this.openingHoursOpen}-${this.openingHoursClose}`)
+      this.editedItem[this.openingHoursEditKey] = sessions.join(',')
+      this.dialogOpeningHoursEntry = false
     },
     save() {
       if (this.editedIndex > -1) {
-        Object.assign(this.libraries[this.editedIndex], this.editedItem);
+        Object.assign(this.libraries[this.editedIndex], this.editedItem)
       } else {
-        this.libraries.push(this.editedItem);
+        this.libraries.push(this.editedItem)
       }
-      this.close();
+      this.close()
     },
     loadLibraries: async function (service) {
-      this.selectedService = service;
-      this.loadingServiceData = true;
-      let self = this;
-      const libraries = await schemaHelper.getSchemaData("libraries", service.code);
+      this.selectedService = service
+      this.loadingServiceData = true
+      let self = this
+      const libraries = await schemaHelper.getSchemaData('libraries', service.code)
       libraries.forEach((e, i) => {
-        Object.keys(e).forEach((key) => {
-          let val = e[key];
-          let newKey = key.replace(/\s+/g, "_");
-          delete libraries[i][key];
-          libraries[i][newKey] = val;
-        });
-      });
-      self.libraries = libraries;
-      this.loadingServiceData = false;
+        Object.keys(e).forEach(key => {
+          let val = e[key]
+          let newKey = key.replace(/\s+/g, '_')
+          delete libraries[i][key]
+          libraries[i][newKey] = val
+        })
+      })
+      self.libraries = libraries
+      this.loadingServiceData = false
     },
     download() {
-      let service = this.selectedService;
-      this.downloadFile(`${service.code}_libraries.csv`, this.libraries);
+      let service = this.selectedService
+      this.downloadFile(`${service.code}_libraries.csv`, this.libraries)
     },
     downloadFile: function (filename, data) {
       var csv = new Blob([Papa.unparse(data)], {
-        type: "text/csv;charset=utf-8;"
-      });
+        type: 'text/csv;charset=utf-8;'
+      })
       if (navigator.msSaveBlob) {
-        navigator.msSaveBlob(csv, filename);
+        navigator.msSaveBlob(csv, filename)
       } else {
         //In FF link must be added to DOM to be clicked
-        var link = document.createElement("a");
-        link.href = window.URL.createObjectURL(csv);
-        link.setAttribute("download", filename);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        var link = document.createElement('a')
+        link.href = window.URL.createObjectURL(csv)
+        link.setAttribute('download', filename)
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
       }
     },
     publishChanges: async function () {
-      let librariesToPublish = JSON.parse(JSON.stringify(this.libraries));
+      let librariesToPublish = JSON.parse(JSON.stringify(this.libraries))
       librariesToPublish.forEach((e, i) => {
-        Object.keys(e).forEach((key) => {
-          let val = e[key];
-          let newKey = key.replace(/_/g, " ");
-          delete librariesToPublish[i][key];
-          librariesToPublish[i][newKey] = val;
-        });
-      });
+        Object.keys(e).forEach(key => {
+          let val = e[key]
+          let newKey = key.replace(/_/g, ' ')
+          delete librariesToPublish[i][key]
+          librariesToPublish[i][newKey] = val
+        })
+      })
       const csvData = new Blob([Papa.unparse(librariesToPublish)], {
-        type: "text/csv;charset=utf-8;"
-      });
+        type: 'text/csv;charset=utf-8;'
+      })
       const saveResult = await schemaHelper.saveSchemaFile(
-        "libraries",
+        'libraries',
         this.selectedService.code,
         csvData,
         this.$store.state.loginKey
-      );
+      )
       if (saveResult) {
-        this.successDialog = true;
+        this.successDialog = true
       } else {
-        this.errorDialog = true;
+        this.errorDialog = true
       }
     },
     preventNav(event) {
       if (!this.isEditing) return
       event.preventDefault()
-      event.returnValue = ""
+      event.returnValue = ''
     }
   },
   components: {
-    "layout-header": Header,
-    "service-select": ServiceSelect,
-    "markdown-section": Markdown
+    'layout-header': Header,
+    'service-select': ServiceSelect,
+    'markdown-section': Markdown
   }
-};
+}
 </script>
 <style scoped></style>

24783285d60c726daa38ca81182e603b0fc892b1
Dave <8709533+DaveBathnes@users.noreply.github.com>
2024-02-29 08:58:45 +0000

    82 ensure all working for live demo (#83)
    * Upgrading packages
    
    * Updating packages
    
    * Updating packages

--- src/components/file-upload.vue
+++ src/components/file-upload.vue
@@ -1,9 +1,20 @@
 <template>
   <v-card elevation="0" color="grey-lighten-5">
-    <v-file-input chips show-size single-line accept="text/csv" counter bg-color="info" density="compact" clearable
-      multiple label="Select a file" :model-value="files" v-on:update:model-value="updateSelection($event)"
-      prepend-inner-icon="mdi-file-document" prepend-icon="" hint="This file loader only accepts CSV files"
-      persistent-hint></v-file-input>
+    <v-file-input
+      chips
+      accept="text/csv"
+      counter
+      variant="outlined"
+      bg-color="white"
+      multiple
+      label="Select a file"
+      :model-value="files"
+      v-on:update:model-value="updateSelection($event)"
+      prepend-inner-icon="mdi-file-document"
+      prepend-icon=""
+      hint="This file loader only accepts CSV files"
+      persistent-hint
+    ></v-file-input>
   </v-card>
 </template>
 
@@ -12,8 +23,8 @@ export default {
   props: ["files"],
   methods: {
     updateSelection(files) {
-      this.$emit('change-files', files)
-    },
+      this.$emit("change-files", files);
+    }
   }
 };
 </script>
